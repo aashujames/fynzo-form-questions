0304ba8be26a363df735fce12179ca5e7ba4427c
Ashish Gupta <ashishgupt25@gmail.com>
2023-01-17 18:41:25 +0530

    feat(Choices): Different type of choices

--- src/Choices/Choices.js
+++ src/Choices/Choices.js
@@ -4,21 +4,63 @@ const Choices = ({ selectedQues, setSelectedChoices }) => {
     const { question_type: type } = selectedQues;
 
     const handleChoice = (choiceId) => {
+        console.log(choiceId);
         setSelectedChoices((selectedChoices) => [
             ...selectedChoices,
             { questionId: selectedQues.id, choiceId }
         ]);
     };
-    console.log(type);
-    return (
-        <div>
-            {selectedQues.choices.map((item) => (
-                <div key={item.id} onClick={() => handleChoice(item.id)}>
-                    {item.choice_text}
-                </div>
-            ))}
-        </div>
-    );
+
+    if (type === 1) {
+        return (
+            <div>
+                {selectedQues.choices.map((item) => (
+                    <div key={item.id}>
+                        <label>
+                            <input
+                                type="radio"
+                                onChange={() => handleChoice(item.id)}
+                            />
+                            {item.choice_text}
+                        </label>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+
+    if (type === 2) {
+        return (
+            <div>
+                <select onChange={(e) => handleChoice(e.target.value)}>
+                    <option>Choose choice</option>
+                    {selectedQues.choices.map((item) => (
+                        <option value={item.id} key={item.id}>
+                            {item.choice_text}
+                        </option>
+                    ))}
+                </select>
+            </div>
+        );
+    }
+
+    if (type === 3) {
+        return (
+            <div>
+                {selectedQues.choices.map((item) => (
+                    <div key={item.id}>
+                        <label>
+                            <input
+                                type="checkbox"
+                                onChange={() => handleChoice(item.id)}
+                            />
+                            {item.choice_text}
+                        </label>
+                    </div>
+                ))}
+            </div>
+        );
+    }
 };
 
 export default Choices;
